2728434a6a215c3fc33827783723a7f1855cf298
Siddhesh Sawant <sawant.sidd@northeastern.edu>
2024-10-11 02:25:19 -0400

    Added files in Model

---
+++ Lab_4/src/model/Product.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author siddheshsawant
+ */
+public class Product {
+    
+    
+    private String name;
+    private int price;
+    private int id;
+
+    private static int count = 0;
+    
+    public Product() {
+        count++;
+        id = count;
+        
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+    
+    
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    
+    @Override
+    public String toString() {
+        return name;
+    }
+    
+}
